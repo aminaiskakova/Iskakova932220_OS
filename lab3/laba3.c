4e91dd275407a6c8c2b5a9d4bc9c1229e10d6734
aminaiskakova <165491406+aminaiskakova@users.noreply.github.com>
2024-12-16 01:24:36 +0700

    Create laba3.c

---
+++ lab3/laba3.c
@@ -0,0 +1,17 @@
+#include <linux/module.h>  
+#include <linux/kernel.h>  
+#include <linux/init.h>    
+
+MODULE_LICENSE("GPL");  
+
+static int __init tsu_module_init(void) {
+    pr_info("Welcome to the Tomsk State University\n"); 
+    return 0;  
+}
+
+static void __exit tsu_module_exit(void) {
+    pr_info("Tomsk State University forever!\n");  
+}
+
+module_init(tsu_module_init);
+module_exit(tsu_module_exit);  
